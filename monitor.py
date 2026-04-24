284757df070c6f08c78861072bb7d7a3c3f9c24c
Hritik <hritikpatel4775@gmail.com>
2026-04-24 21:20:02 +0530

    test-scraper github a

--- monitor.py
+++ monitor.py
@@ -133,6 +133,8 @@ def run_monitor():
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     options.add_argument("--remote-debugging-port=9222")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
 
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service)
